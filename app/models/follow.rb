7d75589abba61eb16de41a8432a9662989561a84
Josh <student@epicodus.com>
2014-04-15 16:32:38 -0700

    Added functionality to follow other users, and only see tweets from people you follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,3 +1,13 @@
 class Follow < ActiveRecord::Base
 belongs_to :user
+
+  def find_handle
+    follow = User.find_by(id: self.stalk_id)
+    follow.handle
+   end
+  def find_tweets
+    follow = User.find_by(id: self.stalk_id)
+    tweets = Tweet.where(user_id: follow.id )
+    tweets
+  end
 end
